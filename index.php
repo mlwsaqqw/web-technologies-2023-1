25567359883821f7b17181c624efe448e29939de
mlwsaqqw <danavin22@mail.ru>
2024-05-23 13:29:59 +0500

    18лаб

--- index.php
+++ index.php
@@ -1,137 +1,114 @@
 <?php
 //1
 echo("<h4>Задание 1</h4>");
-$a = -10;
-$b = -2;
-
-$result = ($a >= 0 && $b >= 0) ? $a - $b : (($a < 0 && $b < 0) ? $a * $b : $a + $b);
-echo $result;
+function evenOddNumbers() {
+    $num = 0;
+    do {
+        if ($num == 0) {
+            echo "$num - это ноль.<br>";
+        } elseif ($num % 2 == 0) {
+            echo "$num - чётное число.<br>";
+        } else {
+            echo "$num - нечётное число.<br>";
+        }
+        $num++;
+    } while ($num <= 10);
+}
+evenOddNumbers();
 
 //2
 echo("<h4>Задание 2</h4>");
-$a = rand(0, 15);
+$regions = array(
+    "Московская область" => array("Москва", "Зеленоград", "Клин"),
+    "Ленинградская область" => array("Санкт-Петербург", "Всеволожск", "Павловск", "Кронштадт"),
+    "Рязанская область" => array("Рязань", "Скопин", "Рыбное")
+);
 
-echo "a = $a<br>"; 
-
-switch ($a) {
-    case 0:
-        echo "0 ";
-    case 1:
-        echo "1 ";
-    case 2:
-        echo "2 ";
-    case 3:
-        echo "3 ";
-    case 4:
-        echo "4 ";
-    case 5:
-        echo "5 ";
-    case 6:
-        echo "6 ";
-    case 7:
-        echo "7 ";
-    case 8:
-        echo "8 ";
-    case 9:
-        echo "9 ";
-    case 10:
-        echo "10 ";
-    case 11:
-        echo "11 ";
-    case 12:
-        echo "12 ";
-    case 13:
-        echo "13 ";
-    case 14:
-        echo "14 ";
-    case 15:
-        echo "15 ";
-        break;
+foreach ($regions as $region => $cities) {
+    echo "$region:<br>";
+    foreach ($cities as $city) {
+        echo "$city, ";
+    }
+    echo "<br>";
 }
-
 
 //3
 echo("<h4>Задание 3</h4>");
-$a = 10;
-$b = 5;
-
-function addition($a, $b) {
-    return $a + $b;
+function transliterate($string) {
+    $translitMap = array(
+        'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd', 'е' => 'e',
+        'ё' => 'yo', 'ж' => 'zh', 'з' => 'z', 'и' => 'i', 'й' => 'j', 'к' => 'k',
+        'л' => 'l', 'м' => 'm', 'н' => 'n', 'о' => 'o', 'п' => 'p', 'р' => 'r',
+        'с' => 's', 'т' => 't', 'у' => 'u', 'ф' => 'f', 'х' => 'x', 'ц' => 'cz',
+        'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shh', 'ъ' => '``', 'ы' => 'y', 'ь' => '`',
+        'э' => 'e`', 'ю' => 'yu', 'я' => 'ya'
+    );
+    $transliterated = '';
+    for ($i = 0; $i < mb_strlen($string); $i++) {
+        $char = mb_substr($string, $i, 1);
+        if (array_key_exists($char, $translitMap)) {
+            $transliterated .= $translitMap[$char];
+        } else {
+            $transliterated .= $char;
+        }
+    }
+    return $transliterated;
 }
-
-function subtraction($a, $b) {
-    return $a - $b;
-}
-
-function multiplication($a, $b) {
-    return $a * $b;
-}
-
-function division($a, $b) {
-    if ($b != 0) {
-        return $a / $b;
-    } else return "Деление на ноль!";
-}
-
-echo "Сумма: " . addition($a, $b) . "<br>";
-echo "Разность: " . subtraction($a, $b) . "<br>";
-echo "Произведение: " . multiplication($a, $b) . "<br>";
-echo "Частное: " . division($a, $b) . "<br>";
-
+$string = "привет мир!";
+echo transliterate($string);
 
 //4
 echo("<h4>Задание 4</h4>");
-function mathOperation($arg1, $arg2, $operation) {
-    switch ($operation) {
-        case 'addition':
-            return addition($arg1, $arg2);
-        case 'subtraction':
-            return subtraction($arg1, $arg2);
-        case 'multiplication':
-            return multiplication($arg1, $arg2);
-        case 'division':
-            return division($arg1, $arg2);
-    }
-}
-echo "Сумма: " . mathOperation($a, $b, 'addition') . "<br>";
-echo "Разность: " . mathOperation($a, $b, 'subtraction') . "<br>";
-echo "Произведение: " . mathOperation($a, $b, 'multiplication') . "<br>";
-echo "Частное: " . mathOperation($a, $b, 'division') . "<br>";
+$menu =  array(
+    array(
+        'title' => 'Главное',
+        'link' => 'menu1',
+        'submenu' => array(
+            array(
+                'title' => 'Контакты',
+                'link' => 'sub-menu1',
+				'submenu' => array(
+                    array(
+                        'title' => 'Наши Соц. сети',
+                        'link' => 'sub-menu2',
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
 
+function createMenu($menu) {
+    echo '<ul>';
+    $count = count($menu);
+    for ($i = 0; $i < $count; $i++) {
+        $value = $menu[$i];
+        echo '<li>';
+        echo "<a href='{$value['link']}'> {$value['title']} </a>";
+        if (isset($value['submenu'])) {
+            createMenu($value['submenu']);
+        }
+        echo '</li>';
+    }
+    echo '</ul>';
+}
+createMenu($menu);
 
 //6
 echo("<h4>Задание 6</h4>");
-function power($val, $pow) {
-    if ($pow === 0) {
-        return 1;
+foreach ($regions as $region => $cities) {
+    echo "$region:<br>";
+    $countCities = false; 
+    foreach ($cities as $city) {
+        if (mb_substr($city, 0, 1) === "К") {
+            echo "$city ";
+            $countCities = true;
+        }
     }
-    elseif ($pow > 0) {
-        return $val * power($val, $pow - 1);
+    if (!$countCities) {
+        echo "нет городов на К";
     }
-	elseif ($pow == 1) {
-        return $val;
-    }
-    else {
-        return 1 / power($val, -$pow);
-    }
+    echo "<br>";
 }
-echo power(2, 3); 
-echo("<br>");
-echo power(5, -2);
-echo("<br>");
-echo power(9, 0);
-echo("<br>");
-echo power(3, 1); 
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>Текущий год: <?php echo date('Y'); ?></p>
-</body>
-</html>
+?>
